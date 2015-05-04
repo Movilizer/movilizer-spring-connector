9a9b049db1a2ab3079974c408de182efaa9a35c2
Jesús de Mula Cano <jesus.demula@movilizer.com>
2015-05-04 08:51:12 +0200

    add classpath finding capabilities

---
+++ src/main/java/com/movilizer/connectors/spring/scanning/services/TriggerExtractor.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright 2015 Movilizer GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.movilizer.connectors.spring.scanning.services;
+
+import com.movilizer.connectors.spring.annotations.MovilizerComponent;
+import com.movilizer.connectors.spring.model.MovilizerAppEndpoint;
+import com.movilizer.connectors.spring.model.exceptions.IllegalTriggerDefinitionException;
+import com.movilizer.connectors.spring.model.triggers.MovilizerTrigger;
+import com.movilizer.connectors.spring.scanning.services.extractors.TriggerAnnotationExtractor;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Service to find a extract the trigger information from a given Movilzer App context.
+ *
+ * @author Jesús de Mula Cano
+ * @see MovilizerComponent
+ * @since 0.1
+ */
+@Component
+public class TriggerExtractor {
+  private static Log logger = LogFactory.getLog(TriggerExtractor.class);
+  private final List<Class<? extends Annotation>> movilizerTriggerAnnotations;
+  private final ClassFinder classFinder;
+  private final List<TriggerAnnotationExtractor> triggerExtractors;
+
+  /**
+   * Create a {@link TriggerExtractor} given a class finder service.
+   * 
+   * @param classFinder to use for finding the {@link MovilizerComponent} classes.
+   */
+  @Autowired
+  public TriggerExtractor(ClassFinder classFinder,
+      List<TriggerAnnotationExtractor> triggerExtractors) {
+    this.classFinder = classFinder;
+    this.triggerExtractors = triggerExtractors;
+    movilizerTriggerAnnotations = new ArrayList<>();
+    for (TriggerAnnotationExtractor triggerExtractor : triggerExtractors) {
+      movilizerTriggerAnnotations.add(triggerExtractor.getAnnotation());
+    }
+  }
+
+  /**
+   * Finds all triggers in the base packages given and defaults the values needed to the endpoints
+   * of the app.
+   *
+   * @param endpoints to use for defaults.
+   * @param basePackages to scan for {@link MovilizerComponent}
+   * @return a map with the triggers of each endpoint.
+   */
+  public Map<MovilizerAppEndpoint, List<MovilizerTrigger>> getAppTriggers(
+      List<MovilizerAppEndpoint> endpoints, Set<String> basePackages) {
+    Map<Class<?>, List<MovilizerTrigger>> classTriggers = new HashMap<>();
+    for (String basePackage : basePackages) {
+      classTriggers.putAll(getTriggersForBasePackage(basePackage, endpoints));
+    }
+    return connectTriggersToEndpoints(endpoints, classTriggers);
+  }
+
+  private Map<Class<?>, List<MovilizerTrigger>> getTriggersForBasePackage(String basePackage,
+      List<MovilizerAppEndpoint> endpoints) {
+    Set<Class<?>> movilizerComponents =
+        classFinder.findClassesAnnotatedWith(MovilizerComponent.class, basePackage);
+    Map<Class<?>, List<MovilizerTrigger>> classTriggers = new HashMap<>();
+    for (Class<?> movilizerComponent : movilizerComponents) {
+      classTriggers
+          .put(movilizerComponent, extractTriggersFromClass(movilizerComponent, endpoints));
+    }
+    return classTriggers;
+  }
+
+  private List<MovilizerTrigger> extractTriggersFromClass(Class<?> movilizerComponent,
+      List<MovilizerAppEndpoint> endpoints) {
+    List<MovilizerTrigger> triggers = new ArrayList<>();
+    for (Method method : movilizerComponent.getMethods()) {
+      List<Annotation> methodAnnotations = Arrays.asList(method.getAnnotations());
+      if (oneOf(methodAnnotations, movilizerTriggerAnnotations)) {
+        for (TriggerAnnotationExtractor triggerExtractor : triggerExtractors) {
+          try {
+            MovilizerTrigger trigger = triggerExtractor.getTrigger(method, endpoints);
+            if (trigger != null) {
+              triggers.add(trigger);
+            }
+          } catch (IllegalTriggerDefinitionException | IllegalAccessException
+              | NoSuchMethodException e) {
+            if (logger.isWarnEnabled()) {
+              logger.warn(String.format("Error while loading trigger '%s' of class %s",
+                  method.getName(), method.getDeclaringClass().getCanonicalName()), e);
+            }
+          }
+        }
+      }
+    }
+    return triggers;
+  }
+
+  private Boolean oneOf(List<Annotation> fromMethod, List<Class<? extends Annotation>> anyOfThese) {
+    for (Annotation annotation : fromMethod) {
+      if (anyOfThese.contains(annotation.getClass())) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private Map<MovilizerAppEndpoint, List<MovilizerTrigger>> connectTriggersToEndpoints(
+      List<MovilizerAppEndpoint> endpoints, Map<Class<?>, List<MovilizerTrigger>> classTriggersMap) {
+
+    Map<MovilizerAppEndpoint, List<MovilizerTrigger>> connectedTriggers = new HashMap<>();
+    if (!endpoints.isEmpty()) {
+      for (List<MovilizerTrigger> movilizerTriggersInClass : classTriggersMap.values()) {
+        for (MovilizerTrigger trigger : movilizerTriggersInClass) {
+          String endpointsPattern = trigger.getFilter().getEndpoint();
+          for (MovilizerAppEndpoint endpoint : endpoints) {
+            if (endpointsPattern.contains(endpoint.getName())) {
+              addTriggerToEndpointInConnectedTriggers(connectedTriggers, endpoint, trigger);
+            }
+          }
+        }
+      }
+    }
+    return connectedTriggers;
+  }
+
+  private void addTriggerToEndpointInConnectedTriggers(
+      Map<MovilizerAppEndpoint, List<MovilizerTrigger>> connectedTriggers,
+      MovilizerAppEndpoint endpoint, MovilizerTrigger trigger) {
+    if (connectedTriggers.containsKey(endpoint)) {
+      connectedTriggers.get(endpoint).add(trigger);
+    } else {
+      List<MovilizerTrigger> newTriggerList = new ArrayList<>();
+      newTriggerList.add(trigger);
+      connectedTriggers.put(endpoint, newTriggerList);
+    }
+  }
+}
---
+++ src/test/java/com/movilizer/connectors/spring/integration/ClasspathAppScannerTest.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright 2015 Movilizer GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.movilizer.connectors.spring.integration;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.notNullValue;
+
+import com.movilizer.connectors.spring.model.MovilizerAppContext;
+import com.movilizer.connectors.spring.model.MovilizerAppEndpoint;
+import com.movilizer.connectors.spring.model.impl.MovilizerAppEndpointImpl;
+import com.movilizer.connectors.spring.model.triggers.MovilizerTrigger;
+import com.movilizer.connectors.spring.scanning.ClasspathAppScanner;
+import com.movilizer.connectors.spring.scanning.services.AppInfoExtractor;
+import com.movilizer.connectors.spring.scanning.services.ClassFinder;
+import com.movilizer.connectors.spring.scanning.services.ConfigExtractor;
+import com.movilizer.connectors.spring.scanning.services.ConfigFinder;
+import com.movilizer.connectors.spring.scanning.services.TriggerExtractor;
+import com.movilizer.connectors.spring.scanning.services.extractors.OnNewDataContainersTriggerExtractor;
+import com.movilizer.connectors.spring.scanning.services.extractors.TriggerAnnotationExtractor;
+import com.movilizer.connectors.spring.testdata.apps.integration.AverageApp;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(JUnit4.class)
+public class ClasspathAppScannerTest {
+
+  Class<?> app = AverageApp.class;
+  String appName = "my-super-app";
+  String appVersion = "PROD";
+  String endpointName = "demo";
+  MovilizerAppEndpoint fakeTestEndpoint;
+
+  String appPackage;
+  ClassFinder classFinder;
+  ConfigFinder configFinder;
+  ConfigExtractor configExtractor;
+  AppInfoExtractor appInfoExtractor;
+  TriggerExtractor triggerExtractor;
+  List<TriggerAnnotationExtractor> triggerExtractors;
+
+  ClasspathAppScanner appScanner;
+
+  @Before
+  public void setUp() throws Exception {
+    appPackage = app.getPackage().getName();
+    classFinder = new ClassFinder();
+    appInfoExtractor = new AppInfoExtractor();
+    configFinder = new ConfigFinder(appInfoExtractor);
+    configExtractor = new ConfigExtractor();
+    TriggerAnnotationExtractor extractor = new OnNewDataContainersTriggerExtractor();
+    triggerExtractors = new ArrayList<>();
+    triggerExtractors.add(extractor);
+    triggerExtractor = new TriggerExtractor(classFinder, triggerExtractors);
+
+    appScanner =
+        new ClasspathAppScanner(classFinder, configFinder, configExtractor, appInfoExtractor,
+            triggerExtractor);
+    appScanner.setAppsBasePackage(appPackage);
+
+    fakeTestEndpoint = new MovilizerAppEndpointImpl(endpointName, 0L, "", "", "", 0, 0);
+  }
+
+  @Test
+  public void testGetApps() throws Exception {
+    List<MovilizerAppContext> apps = appScanner.getApps();
+    assertThat("Apps retrieved should always return a value or empty list", apps,
+        is(notNullValue()));
+    assertThat("There should be one app in the package " + appPackage, apps.size(), is(1));
+
+    MovilizerAppContext app = apps.get(0);
+    assertThat("Apps retrieved should never be null", app, is(notNullValue()));
+    assertThat("The app name should be " + appName, app.getName(), is(appName));
+    assertThat("The app version should properly be " + appVersion, app.getVersion(), is(appVersion));
+    assertThat("The app properties should never be null", app.getProperties(), is(notNullValue()));
+
+    List<MovilizerAppEndpoint> endpoints = app.getEndpoints();
+    assertThat("The app endpoints should never be null", endpoints, is(notNullValue()));
+    assertThat("The app should at least have one endpoint", endpoints.size(), is(greaterThan(0)));
+
+    Map<MovilizerAppEndpoint, List<MovilizerTrigger>> triggersPerEndpoint = app.getTriggers();
+    assertThat("The app triggers per endpoint should never be null", triggersPerEndpoint,
+        is(notNullValue()));
+    assertThat("The app should at least have one endpoint", triggersPerEndpoint.size(),
+        is(greaterThan(0)));
+
+    List<MovilizerTrigger> triggers = app.getTriggers().get(fakeTestEndpoint);
+    assertThat("The app triggers for an endpoint should never be null", triggers,
+        is(notNullValue()));
+    assertThat("The test app triggers for the endpoint " + endpointName
+        + " should have two endpoint", triggers.size(), is(2));
+  }
+}
