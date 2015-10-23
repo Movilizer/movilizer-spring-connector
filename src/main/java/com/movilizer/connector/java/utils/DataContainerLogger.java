1150286d6a91c0625ab3cd043c8b806e00b4f10f
jesus <jesus.demula@movilizer.com>
2015-10-23 13:36:41 +0200

    update datacontainer logger

--- src/main/java/com/movilizer/connector/java/utils/DataContainerLogger.java
+++ src/main/java/com/movilizer/connector/java/utils/DataContainerLogger.java
@@ -15,6 +15,8 @@
 package com.movilizer.connector.java.utils;
 
 import com.movilitas.movilizer.v12.MovilizerUploadDataContainer;
+import com.movilizer.connector.java.MovilizerConnectorAPI;
+import com.movilizer.connector.java.model.Processor;
 import com.movilizer.mds.webservice.services.MovilizerDistributionService;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
@@ -24,88 +26,96 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
-import java.io.*;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.nio.charset.Charset;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Service
 public class DataContainerLogger {
+    private static final Log logger = LogFactory.getLog(DataContainerLogger.class);
 
-//    @Value("${movilizer.charset}")
-//    private String charset;
-//
-//    @Value("${spring.profile.environment}")
-//    private String environment;
-//
-//    @Value("${movilizer.log.dc.dir}")
-//    private String logDirPath;
-//
-//    @Autowired
-//    private MovilizerDistributionService mds;
-//
-////TODO removed  @Autowired
-////  private OXMService serviceOXM;
-//
-//    private static Log logger = LogFactory.getLog(DataContainerLogger.class);
-//
-//    private File dcdir;
-//
-//    int suffix = 0;
-//
-//    String datePath = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss").format(new Date());
-//
-//    @PostConstruct
-//    public void init() {
-//        dcdir = new File(logDirPath);
-//    }
-//
-//    public void log(MovilizerUploadDataContainer container) {
-//        String filename = "dc-" + String.format("%06d", suffix++) + "-" +
-//                container.getContainer().getKey();
-//        writeTextFile(filename, mds.printMovilizerElementToString(container, MovilizerUploadDataContainer.class).getBytes(Charset.forName(charset)));
-//    }
-//
-//    public void writeTextFile(String filename, byte[] content) {
-//        File mqCurrentDir = createLogDirectory();
-//        filename = filename.replace(":", "-");
-//        File tFile = new File(mqCurrentDir, filename + ".xml");
-//        try {
-//            FileOutputStream output = new FileOutputStream(tFile);
-//            IOUtils.write(content, output);
-//            output.close();
-//        } catch (Exception e) {
-//            logger.error("error writing to file " + tFile.getAbsolutePath() + ": ", e);
-//        }
-//    }
-//
-//    private void writeContentToFile(String content, File tFile) throws FileNotFoundException,
-//            UnsupportedEncodingException, IOException {
-//        FileOutputStream fileOutputStream = new FileOutputStream(tFile);
-//        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fileOutputStream, "UTF8"));
-//
-//        out.write(content);
-//        out.close();
-//        fileOutputStream.close();
-//        logger.info("file written to: " + tFile.getAbsolutePath());
-//    }
-//
-//    private File createLogDirectory() {
-//        String pathSuffix = datePath;
-//        if (environment.equals("dev") == false) {
-//            pathSuffix = new SimpleDateFormat("yyyy_MM_dd").format(new Date());
-//        }
-//
-//        File mqCurrentDir = new File(dcdir, pathSuffix);
-//        if (mqCurrentDir.exists() == false) {
-//            try {
-//                suffix = 0;
-//                mqCurrentDir.mkdirs();
-//            } catch (SecurityException e) {
-//                logger.error("Unable to create the MQ Messages directory.", e);
-//            }
-//        }
-//        return mqCurrentDir;
-//    }
+    @Value("${movilizer.charset}")
+    private String charset;
 
+    @Value("${spring.profile.active}")
+    private String environment;
+
+    @Value("${movilizer.datacontainer.dir}")
+    private String logDirPath;
+
+    @Value("${movilizer.datacontainer.log}")
+    private Boolean isActive;
+
+    @Autowired
+    private MovilizerDistributionService mds;
+
+    @Autowired
+    private MovilizerConnectorAPI connector;
+
+    private final DateFormat df = new SimpleDateFormat("yyyy_MM_dd");
+
+    private File loggerBaseFolder;
+    private File currentLoggerFolder;
+    private Long currentSuffix = 0L;
+    private String currentDatePath = df.format(new Date());
+
+    @PostConstruct
+    public void init() {
+        if (isActive) {
+            loggerBaseFolder = new File(logDirPath);
+            connector.registerProcessor(new Processor<MovilizerUploadDataContainer>() {
+                @Override
+                public void process(MovilizerUploadDataContainer container) {
+                    log(container);
+                }
+            }, MovilizerUploadDataContainer.class);
+        }
+    }
+
+    public void log(MovilizerUploadDataContainer container) {
+        String filename = "dc-" + String.format("%06d", currentSuffix++) + "-" + container.getContainer().getKey();
+        if (isNewDay()) {
+            updateCurrentLoggingStatus();
+        }
+        writeTextFile(currentLoggerFolder, filename,
+                mds.printMovilizerElementToString(container, MovilizerUploadDataContainer.class).getBytes(Charset.forName(charset)));
+    }
+
+    protected Boolean isNewDay() {
+        return !df.format(new Date()).equals(currentDatePath);
+    }
+
+    protected void updateCurrentLoggingStatus() {
+        currentSuffix = 0L;
+        currentDatePath = df.format(new Date());
+        currentLoggerFolder = createLogDirectory(loggerBaseFolder, currentDatePath);
+    }
+
+    protected File createLogDirectory(File loggerBaseFolder, String datePath) {
+        File currentDir = new File(loggerBaseFolder, datePath);
+        if (!currentDir.exists()) {
+            try {
+                currentDir.mkdirs();
+            } catch (SecurityException e) {
+                logger.error(String.format("Unable to create the datacontainer logger directory for path %s.",
+                        currentDir.getAbsolutePath()), e);
+            }
+        }
+        return currentDir;
+    }
+
+    protected void writeTextFile(File currentDir, String filename, byte[] content) {
+        filename = filename.replace(":", "-");
+        File tFile = new File(currentDir, filename + ".xml");
+        try {
+            FileOutputStream output = new FileOutputStream(tFile);
+            IOUtils.write(content, output);
+            output.close();
+        } catch (Exception e) {
+            logger.error("Error while writing to file " + tFile.getAbsolutePath() + ": ", e);
+        }
+    }
 }
