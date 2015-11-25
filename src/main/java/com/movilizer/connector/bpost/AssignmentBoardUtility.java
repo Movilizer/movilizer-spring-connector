3dcfcc1a7288c1620762b0e04e0b50059bf20921
Roberto de Miguel <roberto.demiguel@movilizer.com>
2015-11-25 11:44:55 +0100

    added 'd' profile. Added 'd'  wsdl. Modified Assignment board utility to add HASH attribute

--- src/main/java/com/movilizer/connector/bpost/AssignmentBoardUtility.java
+++ src/main/java/com/movilizer/connector/bpost/AssignmentBoardUtility.java
@@ -33,19 +33,23 @@ import com.movilizer.connector.service.client.MovilizerClient;
 @Service
 public class AssignmentBoardUtility {
 
-	private final Log logger = LogFactory.getLog(getClass());
+    private final Log logger = LogFactory.getLog(getClass());
 
-	private static final String HIERARCHY_ATTRIBUTE = "MOVILIZER_ASSIGNMENT_BOARD_HIERARCHY";
+    private static final String HIERARCHY_ATTRIBUTE = "MOVILIZER_ASSIGNMENT_BOARD_HIERARCHY";
+    private static final String HASH_ATTRIBUTE_GROUP = "MOVILIZER_ASSIGNMENT_BOARD_ADDITIONAL";
+    private static final String HASH_ATTRIBUTE = "HASH";
 
-	private static final String GROUP_ATTRIBUTE = "GROUP";
+    private static final String HASH_VALUE = "123456";
 
-	private static final String HIERARCHY_LEVELS[] = { "level-0", "level-1", "level-2", "level-3", "level-4", "level-5",
-			"level-6", "level-7", "level-8" };
+    private static final String GROUP_ATTRIBUTE = "GROUP";
 
-	private static final String GROUP_SEPARATOR = "#";
+    private static final String HIERARCHY_LEVELS[] = {"level-0", "level-1", "level-2", "level-3", "level-4", "level-5",
+            "level-6", "level-7", "level-8"};
 
-	@Autowired
-	CSVUtility utilCSV;
+    private static final String GROUP_SEPARATOR = "#";
+
+    @Autowired
+    CSVUtility utilCSV;
 
     @Autowired
     MovilizerClient movilizer;
@@ -53,19 +57,18 @@ public class AssignmentBoardUtility {
     @Autowired
     OXMUtility utilityOXM;
 
-	@Value("${assignmentconfig.dir}")
-	private String configFilesDir;
+    @Value("${assignmentconfig.dir}")
+    private String configFilesDir;
 
-	@Value("${movilizer.systemId}")
-	private String systemID;
+    @Value("${movilizer.systemId}")
+    private String systemID;
 
 
     public void pushSotiCSV(boolean writeToXML) {
 
         Map<MovilizerParticipant, MovilizerParticipantConfiguration> configurations = createGroupParticipantsFromCSV();
-        if(writeToXML)
-        {
-        	writeConfigurationDataToXMLFile(configurations);
+        if (writeToXML) {
+            writeConfigurationDataToXMLFile(configurations);
         }
         movilizer.assignConfigurationToParticipant(configurations.values());
         movilizer.perfomSyncToCloud();
@@ -76,14 +79,14 @@ public class AssignmentBoardUtility {
         String xml = "";
         for (Map.Entry<MovilizerParticipant, MovilizerParticipantConfiguration> participantEntry : configurations.entrySet()) {
             xml += utilityOXM.toString(new ObjectFactory()
-                    .createMovilizerParticipantConfiguration(participantEntry
-                            .getValue()),
+                            .createMovilizerParticipantConfiguration(participantEntry
+                                    .getValue()),
                     MovilizerParticipantConfiguration.class);
         }
         try {
             writeContentToFile(xml, new File("mov-logs/bpost", "request.xml"));
         } catch (IOException e) {
-        	logger.error("error while writing the gorup config to file", e);
+            logger.error("error while writing the gorup config to file", e);
         }
     }
 
@@ -100,28 +103,30 @@ public class AssignmentBoardUtility {
     }
 
 
-	private  Map<MovilizerParticipant, MovilizerParticipantConfiguration> createGroupParticipantsFromCSV() {
-		Map<MovilizerParticipant, MovilizerParticipantConfiguration> result = new HashMap<MovilizerParticipant, MovilizerParticipantConfiguration>();
-		boolean first = true;
-		for (Entry<String, String[]> entry : utilCSV.createMapFromCSV(configFilesDir + "import.csv").entrySet()) {
-			String deviceAddress = entry.getKey();
-			String name = getPrefixFromEmailDeviceAddress(deviceAddress);
-			String participantKey = deviceAddress.replaceFirst("@", "").replace("+", "");
+    private Map<MovilizerParticipant, MovilizerParticipantConfiguration> createGroupParticipantsFromCSV() {
+        Map<MovilizerParticipant, MovilizerParticipantConfiguration> result = new HashMap<MovilizerParticipant, MovilizerParticipantConfiguration>();
+        boolean first = true;
+        for (Entry<String, String[]> entry : utilCSV.createMapFromCSV(configFilesDir + "import.csv").entrySet()) {
+            String deviceAddress = entry.getKey();
+            String name = getPrefixFromEmailDeviceAddress(deviceAddress);
+            String participantKey = deviceAddress.replaceFirst("@", "").replace("+", "");
 
-			MovilizerParticipant tMovilizerParticipant = new MovilizerParticipant();
-			tMovilizerParticipant.setDeviceAddress(deviceAddress);
-			tMovilizerParticipant.setName(name);
-			tMovilizerParticipant.setParticipantKey(participantKey);
+            MovilizerParticipant tMovilizerParticipant = new MovilizerParticipant();
+            tMovilizerParticipant.setDeviceAddress(deviceAddress);
+            tMovilizerParticipant.setName(name);
+            tMovilizerParticipant.setParticipantKey(participantKey);
 
-			MovilizerParticipantConfiguration movilizerParticipantConf = new MovilizerParticipantConfiguration();
+            MovilizerParticipantConfiguration movilizerParticipantConf = new MovilizerParticipantConfiguration();
 
-			movilizerParticipantConf.setDeviceAddress(deviceAddress);
-			movilizerParticipantConf.setPasswordHashType(PasswordTypes.PLAIN_TEXT_PASSWORD.getValue());
-			movilizerParticipantConf.setPasswordHashValue(entry.getValue()[2]);
-			movilizerParticipantConf.setName(name);
+            movilizerParticipantConf.setDeviceAddress(deviceAddress);
+            movilizerParticipantConf.setPasswordHashType(PasswordTypes.PLAIN_TEXT_PASSWORD.getValue());
+            movilizerParticipantConf.setPasswordHashValue(entry.getValue()[2]);
+            movilizerParticipantConf.setName(name);
 
-			List<Group> groups = createGroupsFromString(entry.getValue()[1], "/");
-			movilizerParticipantConf.getAttributeUpdate().add(getParticipantAttributeUpdateFromGroupString(groups));
+            List<Group> groups = createGroupsFromString(entry.getValue()[1], "/");
+            movilizerParticipantConf.getAttributeUpdate().add(getParticipantAttributeUpdateFromGroupString(groups));
+            //add hash code
+            movilizerParticipantConf.getAttributeUpdate().add(getHashParticipantAttributeUpdate());
 
 			if(first)
 			{
@@ -129,98 +134,112 @@ public class AssignmentBoardUtility {
 				first = false;
 			}
 
-			for (Group group : groups) {
-				MovilizerParticipantGroup participantGroup = new MovilizerParticipantGroup();
-				participantGroup.setName(group.id);
-				movilizerParticipantConf.getGroupAdd().add(participantGroup);
-			}
+            for (Group group : groups) {
+                MovilizerParticipantGroup participantGroup = new MovilizerParticipantGroup();
+                participantGroup.setName(group.id);
+                movilizerParticipantConf.getGroupAdd().add(participantGroup);
+            }
 
-			result.put(tMovilizerParticipant, movilizerParticipantConf);
-		}
-		return result;
-	}
+            result.put(tMovilizerParticipant, movilizerParticipantConf);
+        }
+        return result;
+    }
 
-	private String getPrefixFromEmailDeviceAddress(String email) {
-		String[] participant = email.split("@");
-		return participant[1];
-	}
 
-	private List<Group> createGroupsFromString(String groupString, String separator) {
-		List<Group> result = new ArrayList<Group>();
-		String groupId = "";
-		for (String currentGroup : groupString.split(separator)) {
-			if (currentGroup.isEmpty() == false) {
-				Group group = new Group();
-				group.name = currentGroup;
-				if (groupId.isEmpty() == false) {
-					groupId += GROUP_SEPARATOR;
-				}
-				groupId += currentGroup.replaceAll(" ", "");
-				group.id = groupId;
-				result.add(group);
-			}
-		}
-		return result;
-	}
+    private String getPrefixFromEmailDeviceAddress(String email) {
+        String[] participant = email.split("@");
+        return participant[1];
+    }
 
-	/**
-	 *
-	 * <participantConfiguration deviceAddress="+9990001" > <attributeUpdate
-	 * name="MOVILIZER_ASSIGNMENT_BOARD_HIERARCHY" useIndex="true"> <entry>
-	 * <entry name="COUNTRY"> <entry name="CITY"> <entry name="PART"> <entry
-	 * name="POSTAL_CODE"> <entry name="GROUP"> <valstr>de_MA_LI_68163</valstr>
-	 * </entry> <valstr>68163</valstr> </entry>
-	 * <entry name="GROUP"> <valstr>de_MA_LI</valstr> </entry>
-	 * <valstr>Lindenhof</valstr> </entry>
-	 * <entry name="GROUP"> <valstr>de_MA</valstr> </entry>
-	 * <valstr>Mannheim</valstr> </entry>
-	 * <entry name="GROUP"> <valstr>de</valstr> </entry>
-	 * <valstr>Germany</valstr> </entry> </entry> </attributeUpdate>
-	 * <attributeUpdate name="MOVILIZER_ASSIGNMENT_BOARD_GROUP" useIndex="true">
-	 * <entry> <entry name="NAME"> <valstr>Jamie
-	 * Random</valstr> </entry> </entry> </attributeUpdate>
-	 * <groupAdd name="de"/> <groupAdd name="de_MA"/> <groupAdd name=
-	 * "de_MA_LI"/> <groupAdd name="de_MA_LI_68163"/>
-	 * </participantConfiguration>
-	 *
-	 *
-	 * @param groupString
-	 * @param separator
-	 */
-	private MovilizerAttributeEntry getParticipantAttributeUpdateFromGroupString(List<Group> groups) {
-		MovilizerAttributeEntry entry = new MovilizerAttributeEntry();
-		entry.setName(HIERARCHY_ATTRIBUTE);
-		entry.setUseIndex(true);
-		MovilizerGenericDataContainer nextEntry = new MovilizerGenericDataContainer();
-		entry.setEntry(nextEntry);
-		MovilizerGenericDataContainerEntry currentEntry = null;
+    private List<Group> createGroupsFromString(String groupString, String separator) {
+        List<Group> result = new ArrayList<Group>();
+        String groupId = "";
+        for (String currentGroup : groupString.split(separator)) {
+            if (currentGroup.isEmpty() == false) {
+                Group group = new Group();
+                group.name = currentGroup;
+                if (groupId.isEmpty() == false) {
+                    groupId += GROUP_SEPARATOR;
+                }
+                groupId += currentGroup.replaceAll(" ", "");
+                group.id = groupId;
+                result.add(group);
+            }
+        }
+        return result;
+    }
 
-		int i = 0;
-		for (Group currentGroup : groups) {
-			if (currentEntry == null) {
-				currentEntry = new MovilizerGenericDataContainerEntry();
-				nextEntry.getEntry().add(currentEntry);
-			} else {
-				MovilizerGenericDataContainerEntry tempEntry = new MovilizerGenericDataContainerEntry();
-				currentEntry.getEntry().add(tempEntry);
-				currentEntry = tempEntry;
-			}
-			currentEntry.setName("level-" + i);
-			currentEntry.setValstr(currentGroup.name);
 
-			MovilizerGenericDataContainerEntry groupEntry = new MovilizerGenericDataContainerEntry();
-			groupEntry.setName(GROUP_ATTRIBUTE);
-			groupEntry.setValstr(currentGroup.id);
-			currentEntry.getEntry().add(groupEntry);
 
-			i++;
-		}
-		return entry;
-	}
+    private MovilizerAttributeEntry getHashParticipantAttributeUpdate(){
+        MovilizerAttributeEntry hashAttribute = new MovilizerAttributeEntry();
+        hashAttribute.setName(HASH_ATTRIBUTE_GROUP);
+        MovilizerGenericDataContainerEntry hashEntry = new MovilizerGenericDataContainerEntry();
+        hashEntry.setName(HASH_ATTRIBUTE);
+        hashEntry.setValstr(HASH_VALUE);
+        MovilizerGenericDataContainer container = new MovilizerGenericDataContainer();
+        container.getEntry().add(hashEntry);
+        hashAttribute.setEntry(container);
 
-	private class Group {
-		public String name;
+        return hashAttribute;
+    }
 
-		public String id;
-	}
+    /**
+     * <participantConfiguration deviceAddress="+9990001" > <attributeUpdate
+     * name="MOVILIZER_ASSIGNMENT_BOARD_HIERARCHY" useIndex="true"> <entry>
+     * <entry name="COUNTRY"> <entry name="CITY"> <entry name="PART"> <entry
+     * name="POSTAL_CODE"> <entry name="GROUP"> <valstr>de_MA_LI_68163</valstr>
+     * </entry> <valstr>68163</valstr> </entry>
+     * <entry name="GROUP"> <valstr>de_MA_LI</valstr> </entry>
+     * <valstr>Lindenhof</valstr> </entry>
+     * <entry name="GROUP"> <valstr>de_MA</valstr> </entry>
+     * <valstr>Mannheim</valstr> </entry>
+     * <entry name="GROUP"> <valstr>de</valstr> </entry>
+     * <valstr>Germany</valstr> </entry> </entry> </attributeUpdate>
+     * <attributeUpdate name="MOVILIZER_ASSIGNMENT_BOARD_GROUP" useIndex="true">
+     * <entry> <entry name="NAME"> <valstr>Jamie
+     * Random</valstr> </entry> </entry> </attributeUpdate>
+     * <groupAdd name="de"/> <groupAdd name="de_MA"/> <groupAdd name=
+     * "de_MA_LI"/> <groupAdd name="de_MA_LI_68163"/>
+     * </participantConfiguration>
+     *
+     * @param groups
+     */
+    private MovilizerAttributeEntry getParticipantAttributeUpdateFromGroupString(List<Group> groups) {
+        MovilizerAttributeEntry entry = new MovilizerAttributeEntry();
+        entry.setName(HIERARCHY_ATTRIBUTE);
+        entry.setUseIndex(true);
+        MovilizerGenericDataContainer nextEntry = new MovilizerGenericDataContainer();
+        entry.setEntry(nextEntry);
+        MovilizerGenericDataContainerEntry currentEntry = null;
+
+        int i = 0;
+        for (Group currentGroup : groups) {
+            if (currentEntry == null) {
+                currentEntry = new MovilizerGenericDataContainerEntry();
+                nextEntry.getEntry().add(currentEntry);
+            } else {
+                MovilizerGenericDataContainerEntry tempEntry = new MovilizerGenericDataContainerEntry();
+                currentEntry.getEntry().add(tempEntry);
+                currentEntry = tempEntry;
+            }
+            //TODO
+            currentEntry.setName("level-" + i);
+            currentEntry.setValstr(currentGroup.name);
+
+            MovilizerGenericDataContainerEntry groupEntry = new MovilizerGenericDataContainerEntry();
+            groupEntry.setName(GROUP_ATTRIBUTE);
+            groupEntry.setValstr(currentGroup.id);
+            currentEntry.getEntry().add(groupEntry);
+
+            i++;
+        }
+        return entry;
+    }
+
+    private class Group {
+        public String name;
+
+        public String id;
+    }
 }
